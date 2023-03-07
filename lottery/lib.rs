0b512cb2fc9588e28b850b261b10167501758aae
Piet <piet.wolff@gmail.com>
2023-03-07 17:20:10 +0100

    adding lottery basics and deciding on a winner takes all mechanic

--- lottery/lib.rs
+++ lottery/lib.rs
@@ -2,6 +2,9 @@
 
 #[ink::contract]
 mod lottery {
+    // Import the `Mapping` type
+use ink::storage::Mapping;
+
 
     /// Defines the storage of your contract.
     /// Add new fields to the below struct in order
@@ -9,39 +12,81 @@ mod lottery {
     #[ink(storage)]
     pub struct Lottery {
         /// Stores a single `bool` value on the storage.
-        value: bool,
+        /// used to determine state of the lottery. 
+        /// true meaning tickets can be bought.
+        /// false meaning a closed lottery waiting for the drawing of the winner.
+        open: bool,
+        // Store the winner
+        winner: Option<AccountId>,
+        // Store a mapping from AccountIds to a u32
+        tickets: Mapping<AccountId, u32>,
+        // Amount of funds to be won. Winner takes all.
+        jackpot: u32,
+        // Block of the first ticket purchase
+        starting_block: Option<u32>
+        
+        
     }
+
+
+
+    
 
     impl Lottery {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(init_value: bool ) -> Self {
+            let tickets = Mapping::default();
+            Self { 
+                open: true, 
+                winner: None,
+                tickets,
+                jackpot: 0,
+                starting_block: None
+                
+                 }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
+
+        /// Retrieve the ticket amount of the caller.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn get_tickets(&self) -> Option<u32> {
+            let caller = self.env().caller();
+            self.tickets.get(caller)
         }
 
-        /// Simply returns the current value of our `bool`.
+        /// Retrieve the ticket amount of any account.
         #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
+        pub fn get_tickets_by_account(&self, account: AccountId) -> Option<u32> {
+            self.tickets.get(account)
+        }
+
+        /// Buy Lottery tickets
+        #[ink(message, payable)]
+        pub fn transfer(&mut self) {
+            // set the starting block of the lottery
+            let starting_block = self.env().block_number();
+            let caller = self.env().caller();
+            let tickets = self.tickets.get(caller).unwrap_or(0);
+            let endowment = self.env().transferred_value() as u32;
+            self.tickets.insert(caller, &(tickets + endowment));
+        }
+
+
+        /// Simply returns the current state of the lottery.
+        #[ink(message)]
+        pub fn lottery_state(&self) -> bool {
+            self.open
+        }
+
+        /// Simply returns the current jackpot size of the lottery. This should also represent the amount of tickets that have been purchased so far.
+        #[ink(message)]
+        pub fn jackpot_size(&self) -> u32 {
+            self.jackpot
         }
     }
-
+    /* 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
     /// The below code is technically just normal Rust code.
@@ -53,7 +98,7 @@ mod lottery {
         /// We test if the default constructor does its job.
         #[ink::test]
         fn default_works() {
-            let lottery = Lottery::default();
+            let lottery = Lottery::new(true);
             assert_eq!(lottery.get(), false);
         }
 
@@ -65,5 +110,5 @@ mod lottery {
             lottery.flip();
             assert_eq!(lottery.get(), true);
         }
-    }
+    }*/
 }
